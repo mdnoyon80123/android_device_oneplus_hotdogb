bd0865b194fc3fb8e630d79b97923cd0c9ffdc90
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-04-20 07:45:47 +0530

    hotdogb: Lunaris Bringup (Add Maintainer prop)

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Lunaris Maintainer prop
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.lunaris.maintainer=Shipu
+
 # Device init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
